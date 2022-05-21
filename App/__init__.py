41b8dc426c13d2b79db4e9e309178269efd9809f
Rony Magalhaes <ronygustavo_rm@yahoo.com.br>
2022-05-21 11:36:45 -0300

    RGMNutri

--- App/__init__.py
+++ App/__init__.py
@@ -5,24 +5,13 @@ from sqlalchemy import create_engine
 from flask_migrate import Migrate
 from googletrans import Translator
 
-from flask_script import Manager
-
 app = Flask(__name__)
 db = SQLAlchemy(app,session_options={"autoflush": False})
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 translator = Translator()
-#try:
-#    with engine.connect() as con:
-#        sql = open('App\sql\Triggers_sisnutri.sql','r').read()
-#        rs = con.execute(sql)
-#        print(rs)
-#except:
-#    pass
 
 migrate = Migrate(app,db)
 
-#manager = Manager(app)
-#manager.add_command('db',MigrateCommand)
 
 app.config.from_object('config')
 db.init_app(app)
