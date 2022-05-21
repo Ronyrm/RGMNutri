69e1e5723a4a2869328843b6e5da4f5967b4e8eb
Rony Magalhaes <ronygustavo_rm@yahoo.com.br>
2022-05-21 11:44:34 -0300

    RGMNutri

--- App/views/several.py
+++ App/views/several.py
@@ -2,11 +2,6 @@ import json
 from flask import jsonify
 from config import UPLOAD_FOLDER
 import requests
-from IPython.core.display import display
-from bs4 import BeautifulSoup
-import  pandas as pd
-from  pandas_datareader  import  data as web
-import  matplotlib.pyplot as plt
 import re
 import datetime
 from App import translator
@@ -21,14 +16,6 @@ def search_dados_temperatura_lat_lon(lat,lon,units):
         return req.json()
     except:
         return None
-
-    #try:
-    #    import pyowm
-    #    owm = pyowm.OWM(chavekey)
-    #    obs = owm.weather_at_place('Muriaé,BR')
-    #    return obs.get_weather()
-    #except:
-    #    return None
 
 
 # -------------------------- BUSCA DADOS TEMPERATURA POR LATITUDE E LONGITUDE --------------------------
@@ -66,57 +53,6 @@ def search_latitude_longitude_geolocator(strcyte):
         return result
     except:
         return None
-
-
-# -------------------------- RASTREIA OBJETO DOS CORREIOS VIA CODIGO = RETURN JSON --------------------------
-def search_tracker_correios(codigo):
-    from App.views.packagetrack import update_status_track
-    req = requests.post('https://www2.correios.com.br/sistemas/rastreamento/ctrl/ctrlRastreamento.cfm?',
-                        data={'objetos': codigo})
-
-    soup = BeautifulSoup(req.text, 'html.parser')
-    i = 0
-    data = {}
-
-    soupinfo = soup.find(attrs={"class": "info alert"})
-    if soupinfo != None:
-        text = re.sub('\n', " ", soupinfo.text)
-        text = re.sub('\r', ' ', text)
-        text = re.sub('\t', ' ', text)
-        newdict = {0: text.strip()+' Ou código inválido!'}
-        data.update(newdict)
-        update_status_track(codigo, 'N')
-        return {'data': data,'tot':1}
-
-    for soupchildren in soup.find_all(attrs={"class": "listEvent sro"}):
-        j = 1
-        newdict = {i:{}}
-        for td in soupchildren.find_all("td"):
-            newdict[i].update({j:{}})
-
-            text = re.sub('\n'," ",td.text)
-            text = re.sub('\r',' ',text)
-            text = re.sub('\t', ' ', text)
-
-            text = re.sub('       ',' - ',text)
-            text = re.sub('Clique aqui Minhas Importações',' ',text)
-            text = re.sub('Informar nº do documento para a fiscalização e entrega do seu objeto',
-                          '<br>Caso não informou o nº do documento <a target="_blank" href="https://www2.correios.com.br/sistemas/rastreamento/default.cfm?objetos='+codigo+'">Clique Aqui</a>',text)
-            newdict[i][j] = text
-            j+=1
-        data.update(newdict)
-
-
-        i+=1
-    import json
-    tot = len(data)
-
-    if data[0][2].strip() == ' Objeto entregue ao destinatário  '.strip():
-        update_status_track(codigo,'E')
-    else:
-        update_status_track(codigo, 'P')
-
-    return {'data':data,'tot':tot}
 
 
 # -------------------------- TRADUZ DETERMINADO TEXTO EM INGLES OU VICE-VERSA EM PORTUGUES = RETURN JSON --------------------------
@@ -168,55 +104,3 @@ def sendsms_twilio(msgsms,phone):
                                       body=msgsms)
     result = mensagem.sid()
     return  result
-
-
-# -------------------------- BUSCA COTAÇÕES BOLSA DE VALORES --------------------------
-
-def extract_html_in_text_source(url):
-    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
-  "X-Requested-With": "XMLHttpRequest"}
-    source=requests.get(url, headers=headers).text
-    return source
-
-
-def search_cotacoes_bolsa_valores():
-    url = 'https://www.fundamentus.com.br/resultado.php'
-
-    # url = 'https://www.infomoney.com.br/cotacoes/ibovespa'
-    #url = 'https://br.financas.yahoo.com/noticias/acoes-mais-negociadas?_device=desktop&device=desktop&failsafe=1&ynet=0&offset=0&count=1000'
-    resp = extract_html_in_text_source(url)
-    # soup = BeautifulSoup(req.text, 'html.parser')
-    df = pd.read_html(resp, decimal=',', thousands='.', encoding="UTF-8")[0]
-    resjson = df.to_json()
-
-    for row in range(0, len(df.values)):
-        item = df.values[row]
-        print(item)
-    return  json.loads(resjson)
-
-def finance_cotacao(name):
-    import os
-    start = datetime.datetime(2021,9,1)
-    end = datetime.datetime(2021,9,30)
-    listname = name.split(',')
-    df = web.DataReader(listname,data_source='yahoo',start=start,end=end)
-
-    display(df)
-    df['Adj Close'].plot(figsize=(15,10))
-
-    spath = os.path.join(UPLOAD_FOLDER, f'grafic_{name}.png')
-    if os.path.exists(spath):
-        os.remove(spath)
-
-    plt.savefig(spath)
-    #generate_qrcode_myzap()
-    resultjson = df.to_json()
-    resultjson = json.loads(resultjson)
-    return resultjson
-
-
-def generate_qrcode_myzap():
-    import qrcode
-    data = 'https://api.whatsapp.com/send?phone=5532984422783'
-    img = qrcode.make(data)
-    img.save(UPLOAD_FOLDER+'qrcode_rony.png')
